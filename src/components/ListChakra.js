aa4111f201612ac688d30a3e39e271defdeea166
adi_yustian <adiyusri95@gmail.com>
2022-07-07 09:27:54 +0700

    list content practice

---
+++ src/components/ListChakra.js
@@ -0,0 +1,180 @@
+import { Box, Text, Button } from "@chakra-ui/react";
+
+function ListChakra() {
+    return (
+        <Box>
+        <Box
+            border="2px"
+            borderColor="gray.200"
+            //alignItems="center"
+            display="flex"
+            rounded="lg"
+            maxWidth="75%"
+            mx="auto"
+            pt={4}
+            pl={4}
+            pb={4}
+            pr={4}
+            mt={4}
+        >
+            <Box flex={1}>
+            <Text
+                fontWeight="bold"
+                fontSize="2xl"
+            >
+                Lorem Ipsum Sit Dolor Amet
+            </Text>
+            <Text 
+                fontWeight="light"
+                fontSize="sm"
+            >
+                28/06/2022 12:33
+            </Text>
+            </Box>
+            <Box>
+            <Button mr={4} colorScheme="blue">Detail</Button>
+            <Button mr={4} colorScheme="teal">Edit</Button>
+            <Button colorScheme={"red"}>Delete</Button>
+            </Box>
+        </Box>
+
+        <Box
+            border="2px"
+            borderColor="gray.200"
+            //alignItems="center"
+            display="flex"
+            rounded="lg"
+            maxWidth="75%"
+            mx="auto"
+            pt={4}
+            pl={4}
+            pb={4}
+            pr={4}
+            mt={4}
+        >
+            <Box flex={1}>
+            <Text
+                fontWeight="bold"
+                fontSize="2xl"
+            >
+                Lorem Ipsum Sit Dolor Amet
+            </Text>
+            <Text 
+                fontWeight="light"
+                fontSize="sm"
+            >
+                28/06/2022 12:33
+            </Text>
+            </Box>
+            <Box>
+            <Button mr={4} colorScheme="blue">Detail</Button>
+            <Button mr={4} colorScheme="teal">Edit</Button>
+            <Button colorScheme={"red"}>Delete</Button>
+            </Box>
+        </Box>
+        <Box
+            border="2px"
+            borderColor="gray.200"
+            //alignItems="center"
+            display="flex"
+            rounded="lg"
+            maxWidth="75%"
+            mx="auto"
+            pt={4}
+            pl={4}
+            pb={4}
+            pr={4}
+            mt={4}
+        >
+            <Box flex={1}>
+            <Text
+                fontWeight="bold"
+                fontSize="2xl"
+            >
+                Lorem Ipsum Sit Dolor Amet
+            </Text>
+            <Text 
+                fontWeight="light"
+                fontSize="sm"
+            >
+                28/06/2022 12:33
+            </Text>
+            </Box>
+            <Box>
+            <Button mr={4} colorScheme="blue">Detail</Button>
+            <Button mr={4} colorScheme="teal">Edit</Button>
+            <Button colorScheme={"red"}>Delete</Button>
+            </Box>
+        </Box>
+        <Box
+            border="2px"
+            borderColor="gray.200"
+            //alignItems="center"
+            display="flex"
+            rounded="lg"
+            maxWidth="75%"
+            mx="auto"
+            pt={4}
+            pl={4}
+            pb={4}
+            pr={4}
+            mt={4}
+        >
+            <Box flex={1}>
+            <Text
+                fontWeight="bold"
+                fontSize="2xl"
+            >
+                Lorem Ipsum Sit Dolor Amet
+            </Text>
+            <Text 
+                fontWeight="light"
+                fontSize="sm"
+            >
+                28/06/2022 12:33
+            </Text>
+            </Box>
+            <Box>
+            <Button mr={4} colorScheme="blue">Detail</Button>
+            <Button mr={4} colorScheme="teal">Edit</Button>
+            <Button colorScheme={"red"}>Delete</Button>
+            </Box>
+        </Box><Box
+            border="2px"
+            borderColor="gray.200"
+            //alignItems="center"
+            display="flex"
+            rounded="lg"
+            maxWidth="75%"
+            mx="auto"
+            pt={4}
+            pl={4}
+            pb={4}
+            pr={4}
+            mt={4}
+        >
+            <Box flex={1}>
+            <Text
+                fontWeight="bold"
+                fontSize="2xl"
+            >
+                Lorem Ipsum Sit Dolor Amet
+            </Text>
+            <Text 
+                fontWeight="light"
+                fontSize="sm"
+            >
+                28/06/2022 12:33
+            </Text>
+            </Box>
+            <Box>
+            <Button mr={4} colorScheme="blue">Detail</Button>
+            <Button mr={4} colorScheme="teal">Edit</Button>
+            <Button colorScheme={"red"}>Delete</Button>
+            </Box>
+        </Box>
+        </Box>
+    );
+}
+
+export default ListChakra;
